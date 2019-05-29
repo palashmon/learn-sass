14eeb8ada74ddbd7e2e664462dd85769ce7aa6a6
Palash Mondal <palashm@live.in>
2019-05-30 00:04:54 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# learn-sass
-Playground to learn and practice more about SASS
+# Learn [Sass](https://sass-lang.com/)
+
+This repo is a simple playground to learn and practice more about SASS.
+
+Sass is a stylesheet language that’s compiled to CSS. It allows you to use variables, nested rules, mixins, functions, and more, all with a fully CSS-compatible syntax. Sass helps keep large stylesheets well-organized and makes it easy to share design within and across projects.
+
+## Usage
+
+### Installation
+
+Install the dependencies (gulp, gulp-sass, browser-sync)
+
+```sh
+npm install
+```
+
+### Run
+
+This will watch your sass files, compile them and run your dev server at http://localhost:3000
+
+```sh
+npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+  return gulp
+    .src(['src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+// Watch Sass & Serve
+gulp.task(
+  'serve',
+  gulp.series('sass', function() {
+    browserSync.init({
+      server: './src'
+    });
+
+    gulp.watch(['src/scss/*.scss'], gulp.series('sass'));
+    gulp.watch('src/*.html').on('change', browserSync.reload);
+  })
+);
+
+// Default Task
+gulp.task('default', gulp.series('serve'));
